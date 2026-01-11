b86b53af4274bced83888009587c6f0699edafc1
Reza Kalhor <r3za.kalhor@gmail.com>
2026-01-11 00:52:10 -0500

    logging and exception

--- src/logger.py
+++ src/logger.py
@@ -4,12 +4,15 @@ from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO,
+    level=logging.INFO
 )
+
+if __name__ == "__main__":
+    logging.info("Logging setup complete.")
