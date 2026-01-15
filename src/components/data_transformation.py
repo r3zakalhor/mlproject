5b7bfa71d775fc2598bf5e27508368297f2412be
Reza Kalhor <r3za.kalhor@gmail.com>
2026-01-15 16:41:03 -0500

    prediction pipline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -43,11 +43,12 @@ class DataTransformation:
 
             cat_pipeline = Pipeline(
                 steps=[
-                    ("imputer", SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder", OneHotEncoder()),
+                    ("imputer", SimpleImputer(strategy="most_frequent", missing_values=None)),
+                    ("one_hot_encoder", OneHotEncoder(handle_unknown="ignore")),
                     ("scaler", StandardScaler(with_mean=False))
                 ]
             )
+
             logging.info("Numerical and categorical pipelines created")
 
             preprocessor = ColumnTransformer(
